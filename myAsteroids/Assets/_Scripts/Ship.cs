10c48de5506a850bafbd803d5edb5221ebf54cde
Andrew <makarGames@protonmail.com>
2021-06-27 21:02:18 +0300

    Initial commit

---
+++ myAsteroids/Assets/_Scripts/Ship.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] [Range(0f, 10f)] private float _movingSpeed = 1f;
+    [SerializeField] [Range(0f, 10f)] private float _turnSpeed = 1f;
+
+    private bool _moving;
+    private float _turnDirection;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _moving = Input.GetAxis("Vertical") > 0f;
+        _turnDirection = -Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (_moving)
+        {
+            Moving(_movingSpeed);
+        }
+
+        if (_turnDirection != 0f)
+        {
+            Rotation(_turnDirection, _turnSpeed);
+        }
+    }
+
+    private void Moving(float speed)
+    {
+        _rigidbody.AddForce(transform.up * speed);
+    }
+
+    private void Rotation(float direction, float speed)
+    {
+        _rigidbody.AddTorque(direction * speed);
+    }
+}
